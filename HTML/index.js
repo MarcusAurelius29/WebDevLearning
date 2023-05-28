124d9159f11734aa1bb1e4121f61f51c33f9e3e6
MarcusAurelius29 <varungole1999@gmail.com>
2023-05-28 11:44:41 +0530

    javascript

---
+++ HTML/index.js
@@ -0,0 +1,25 @@
+console.log('Hello, World!');
+
+let name = "Varun";
+console.log(name);
+
+const interestRate = 0.3;
+console.log(interestRate);
+
+let person = {
+name: "Varun",
+age: 30
+};
+
+person.name= "John";
+
+person["name"] = "Ashish"
+
+console.log(person.name); // Output is Ashish
+
+
+function greet() {
+    console.log("Hello, World!");
+}
+
+greet();
